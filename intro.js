4529ddde5dfee3670f3011e5c731f69af1c64ddb
Chetam Agbodike <agbodikechetty@gmail.com>
2026-04-10 09:05:23 -0600

    My Assignment

---
+++ intro.js
@@ -0,0 +1,10 @@
+// Defining our variables
+const name = "Agbodike Chetam";
+const age = 27;
+const favLanguage = "JavaScript";
+
+// Logging the information to the console
+console.log("--- My Profile ---");
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favourite Language: " + favLanguage);
